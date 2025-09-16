bf891a3cf9b44e66f4d7701ad40e147b4536d6de
regular-student <alanhacase@gmail.com>
2025-09-16 19:42:20 -0300

    fix

--- app.py
+++ app.py
@@ -1,28 +1,30 @@
 # app.py
 
 from flask import Flask, render_template, request, redirect, url_for
-from models.personagem import Personagem, Guerreiro, Mago, Clerigo, Humano, Elfo, Halfling
+from models.personagem import (
+    Personagem,
+    Guerreiro,
+    Mago,
+    Clerigo,
+    Humano,
+    Elfo,
+    Halfling,
+)
 from services.distribuicao_atributos import determinar_atributos
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "sua-chave-secreta-aqui" # Não sei ainda
+app.config["SECRET_KEY"] = "sua-chave-secreta-aqui"  # Não sei ainda
 
-# Dicionários 
-CLASSES = {
-    "1": Guerreiro,
-    "2": Mago,
-    "3": Clerigo
-}
+# Dicionários
+CLASSES = {"1": Guerreiro, "2": Mago, "3": Clerigo}
 
-RACAS = {
-    "1": Humano,
-    "2": Elfo,
-    "3": Halfling
-}
+RACAS = {"1": Humano, "2": Elfo, "3": Halfling}
+
 
 @app.route("/")
 def index():
     return redirect(url_for("criar_personagem"))
+
 
 @app.route("/criar", methods=["GET", "POST"])
 def criar_personagem():
@@ -38,7 +40,9 @@ def criar_personagem():
 
         if not classe_escolhida or not raca_escolhida:
             # Lida com uma escolha inválida
-            return render_template("criar_personagem.html", erro="Opção de classe ou raça inválida!")
+            return render_template(
+                "criar_personagem.html", erro="Opção de classe ou raça inválida!"
+            )
 
         atributos_base = determinar_atributos(estilo)
 
@@ -47,6 +51,7 @@ def criar_personagem():
         return render_template("personagem.html", personagem=personagem)
 
     return render_template("criar_personagem.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
